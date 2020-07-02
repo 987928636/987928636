5ef64e7bdfdd15619a731420f23259b1993a0d71
yueyuye <987928636@qq.com>
2020-07-02 18:27:58 +0800

    Site updated: 2020-07-02 18:27:57

--- js/src/next.js
+++
@@ -1,6 +0,0 @@
-<!-- 页面点击小红心 -->
-<script type="text/javascript" src="/czwBlog/js/src/love.js"></script>
-<!--背景特效添加   下雪（该特效不好兼容手机） 点击彩带  自动彩带   -->
-<!--<script type="text/javascript" src="/czwBlog/js/src/snow.js"></script>-->
-<script type="text/javascript" src="/czwBlog/js/src/ribbon.js"></script>
-<!--<script type="text/javascript" src="/czwBlog/js/src/ribbon1.js"></script>-->
